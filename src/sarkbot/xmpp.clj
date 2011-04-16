7cf000085fb3141e7d1f4a5c4f628de1fe0e394f
Aaron Kuehler <aaronkuehler@gmail.com>
2011-04-16 00:35:23 -0400

    Almost able to respond to a message

--- src/sarkbot/xmpp.clj
+++ src/sarkbot/xmpp.clj
@@ -1,5 +1,54 @@
 (ns sarkbot.xmpp)
 
+(defonce +chat-message-type+ org.jivesoftware.smack.packet.Message$Type/chat)
+
+(defonce +chat-filter+ (org.jivesoftware.smack.filter.MessageTypeFilter.
+		    +chat-message-type+))
+
+(defn message-to-map [message]
+  "Breaks apart a message into a map of its interesting parts."
+  {:to (.getTo message)
+   :from (.getFrom message)
+   :subject (.getSubject message)
+   :body (.getBody message)})
+
+(defn make-response [orig-message-map response-body]
+  "Create a response to a message."
+  (let [to ( org.jivesoftware.smack.util.StringUtils/parseBareAddress (:from orig-message-map))
+	from (:to orig-message-map)
+	message (org.jivesoftware.smack.packet.Message. to +chat-message-type+)]
+    (.setTo message to)
+    (.setFrom message from)
+    (.setBody message response-body)
+    message))
+
+(defn respond [connection orig-message-map response-body]
+  "Responds to a message."
+  (let [response (make-response orig-message-map response-body)]
+    (try
+      (println "Responding to message from: " (.getTo response) " " response-body)
+      (.sendPacket connection response)
+      (catch Exception exception
+	(println exception)))))
+
+(defn make-packet-listener [handler]
+  "Make a packet listener using the handler as the execution body."
+  (proxy [org.jivesoftware.smack.PacketListener]
+      []
+    (processPacket [packet]
+      (println "Handling message: " (message-to-map packet))
+      (try
+	(handler (message-to-map packet))
+	(catch Exception exception
+	  (println exception))))))
+
+(defn add-message-handler [connection handler]
+  "Adds a handler to the connection to process inbound messages."
+  (try
+    (.addPacketListener connection (make-packet-listener handler) +chat-filter+)
+    (catch Exception exception
+      (println exception))))
+
 (defn make-connection-config [config]
   "Creates a configuration holder object for a connection endpoint."
   (let [host (:host config)
@@ -7,20 +56,27 @@
 	domain (:domain config)]
     (org.jivesoftware.smack.ConnectionConfiguration. host port domain)))
 
-(defn make-connection [config]
-  "Creates a connection to the endpoint described by the configuration."
+(defn make-xmpp-connection [config]
+  "Creates a XMPP connection to the endpoint described by the configuration."
   (let [connection-config (make-connection-config config)]
-    (org.jivesoftware.smack.XMPPConnection. connection-config)))
+     (org.jivesoftware.smack.XMPPConnection. connection-config)))
 
-(defn connect [config]
+(defn make-connection [config]
   "Creates an open connection the service described in the cofiguration."
-  (let [connection (make-connection config)
+  (let [connection (make-xmpp-connection config)
 	username (:username config)
 	password (:password config)]
-    (.connect connection)
-    (.login connection username password)
-    connection))
+    (try
+      (.connect connection)
+      (.login connection username password)
+      connection
+      (catch Exception exception
+	(println exception)))))
 
 (defn disconnect [connection]
   "Disconnects from the service."
   (.disconnect connection))
+
+(defn stdout-handler [message]
+  "Dumps the body of a message to STDOUT."
+  (println (:body message)))
