dc7f0f4a54f485a21ada163799522e548109e7a0
Aaron Kuehler <aaronkuehler@gmail.com>
2011-04-19 11:42:45 -0400

    Allow the echo command to respond to collections of words.

--- src/sarkbot/commands.clj
+++ src/sarkbot/commands.clj
@@ -1,12 +1,13 @@
-(ns sarkbot.commands)
+(ns sarkbot.commands
+  (:require [clojure.contrib.string :as string-util]))
 
 (defn now []
   "Returns the current server date and time."
   (java.util.Date.))
 
-(defn echo [content]
+(defn echo [& content]
   "Simply returns the content."
-  content)
+  (string-util/join " " content))
 
 (def commands {:echo echo
 	       :time now})
