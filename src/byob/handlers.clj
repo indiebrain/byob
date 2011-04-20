acdf47d91363c38c92328695737df499c967c53e
Aaron Kuehler <aaronkuehler@gmail.com>
2011-04-20 12:21:44 -0400

    The command-handler should discard a message if no body is supplied rather than NPE on message body access.

--- src/byob/handlers.clj
+++ src/byob/handlers.clj
@@ -11,9 +11,10 @@
 (defn command-handler
   "Interrogates a message for an instruction from the sender and attempts to execute the instruction if it is recognized."
   [connection message]
-  (let [message-body (:body message)
-	[command & args] (string-util/split #"\s+" message-body)
-	command-function (cmd/commands (symbol command))]
-    (if (nil? command-function)
-      (xmpp/send-chat connection message (str "I don't know how to interpret the command: " (symbol command)))
-      (xmpp/send-chat connection message (str (apply command-function args))))))
+  (if (not (nil? (:body message)))
+    (let [message-body (:body message)
+	  [command & args] (string-util/split #"\s+" message-body)
+	  command-function (cmd/commands (symbol command))]
+      (if (nil? command-function)
+	(xmpp/send-chat connection message (str "I don't know how to interpret the command: " (symbol command)))
+	(xmpp/send-chat connection message (str (apply command-function args)))))))
